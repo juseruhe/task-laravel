164c3357726ba3b06770c9c5499ec2c39119fd35
Sebastian <jsruiz7411@misena.edu.co>
2020-10-28 13:15:51 -0500

    se hace migracion con tasks

--- database/migrations/2020_10_28_173456_create_tasks_table.php
+++ database/migrations/2020_10_28_173456_create_tasks_table.php
@@ -15,7 +15,12 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+           $table->string('name');
+            $table->text('description');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
+
         });
     }
 
