634051d201e0911c80546f5a5568139539bf45ed
Sebastian <jsruiz7411@misena.edu.co>
2020-10-28 12:41:11 -0500

    cambios

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+       Laraveles\Spanish\SpanishServiceProvider::class,
+
     ],
 
     /*
